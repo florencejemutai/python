5df09dac127afb4cfcf104e2dbd1657fe9a9d8cc
florencejemutai <160845670+florencejemutai@users.noreply.github.com>
2024-02-28 20:49:08 +0300

    Add files via upload
    persona;ized message

---
+++ user_input.py
@@ -0,0 +1,9 @@
+#asking for users name
+
+name = input("What is your name? ")
+
+age = input("How old are you? ")
+
+location = input("where are you located? ")
+
+print(f"you are called {name} and you are {age} years old living in {location} . ")
